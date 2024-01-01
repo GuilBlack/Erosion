1a84d51d8d8b0a323472faf8cbbcb65be21f2a59
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2024-01-01 16:53:32 +0100

    remove grid erosion code

--- Assets/Code/TerrainGenerator.cs
+++ Assets/Code/TerrainGenerator.cs
@@ -20,29 +20,7 @@ public class TerrainGenerator : MonoBehaviour
         public float Scale;
     };
 
-    [Serializable]
-    public struct CellData
-    {
-        public float Height;
-        public float WaterHeight;
-        public float Sediment;
-        public float Hardness;
-        public Vector4 WaterOutflowFlux;
-        public Vector2 Velocity;
-
-        public override string ToString()
-        {
-            return Height.ToString() + '\n' +
-                WaterHeight.ToString() + '\n' +
-                Sediment.ToString() + '\n' +
-                Hardness.ToString() + '\n' +
-                WaterOutflowFlux.ToString("F7") + '\n' +
-                Velocity.ToString("F7");
-        }
-    };
-
     [SerializeField] private ComputeShader _heightmapCompute;
-    [SerializeField] private ComputeShader _erosionCompute;
     [SerializeField] private int _mapSize = 1024;
     [SerializeField] private Texture2D _heightmapTexture;
     [SerializeField] private FBMParameters _fbmParams;
@@ -51,8 +29,6 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private int _floatToIntScalar = 1000000000;
     private int[] _globalMinMaxInt = new int[2] { 0, int.MaxValue };
     private float[,] _heightmap2D;
-    private List<CellData[]> _cellDataBuffers = new List<CellData[]>(2) { new CellData[0], new CellData[0] };
-    private int _currentBuffer = 0;
 
     public void ComputeHeightmap()
     {
@@ -144,8 +120,6 @@ public class TerrainGenerator : MonoBehaviour
         int processorCount = Environment.ProcessorCount;
         int chunkSize = _mapSize / processorCount;
         _heightmap2D = new float[_mapSize, _mapSize];
-        _cellDataBuffers[0] = new CellData[_mapSize * _mapSize];
-        _cellDataBuffers[1] = new CellData[_mapSize * _mapSize];
         Color[] data = _heightmapTexture.GetPixels();
 
         Parallel.For(0, processorCount, chunkIndex =>
@@ -159,74 +133,7 @@ public class TerrainGenerator : MonoBehaviour
                 {
                     float height = data[i * _mapSize + j].r;
                     _heightmap2D[i, j] = height;
-                    _cellDataBuffers[0][i * _mapSize + j].Height = height;
-                    _cellDataBuffers[0][i * _mapSize + j].Hardness = 0.2f;
-                    _cellDataBuffers[1][i * _mapSize + j].Height = height;
-                    _cellDataBuffers[1][i * _mapSize + j].Hardness = 0.2f;
                 }
-            }
-        });
-    }
-
-    public void ErodeTerrain()
-    {
-        int kernelHandle = _erosionCompute.FindKernel("ErosionMain");
-
-        for (int i = 0; i < 5; i++)
-            Debug.Log(_cellDataBuffers[_currentBuffer][i + _mapSize].ToString());
-
-        ComputeBuffer[] computeBuffers = new ComputeBuffer[2] 
-        { 
-            new ComputeBuffer(_mapSize * _mapSize, sizeof(float) * (4 + 4 + 2)), 
-            new ComputeBuffer(_mapSize * _mapSize, sizeof(float) * (4 + 4 + 2)) 
-        };
-        computeBuffers[_currentBuffer].SetData(_cellDataBuffers[_currentBuffer]);
-        computeBuffers[(_currentBuffer + 1) % 2].SetData(_cellDataBuffers[(_currentBuffer + 1) % 2]);
-
-        #region Set Erosion Parameters
-        _erosionCompute.SetFloat("MapSize", _mapSize);
-        _erosionCompute.SetFloat("RainRate", 0.00001f);
-        _erosionCompute.SetFloat("Kr", 1f);
-        _erosionCompute.SetFloat("Kc", 0.0001f);
-        _erosionCompute.SetFloat("KdMax", 0.002f);
-        _erosionCompute.SetFloat("DeltaTime", 0.02f);
-        _erosionCompute.SetFloat("Gravity", 9.81f);
-        _erosionCompute.SetFloat("CrossArea", 1f);
-        _erosionCompute.SetFloat("EvaporationRate", 0.02f);
-        _erosionCompute.SetFloat("SoilSuspendedRate", 0.5f);
-        _erosionCompute.SetFloat("SedimentDepositionRate", 0.5f);
-        _erosionCompute.SetFloat("SedimentSofteningRate", 5f);
-        #endregion
-
-        for (int i = 0; i < 1000; i++)
-        {
-            // Update the compute buffer data for current iteration
-            _erosionCompute.SetBuffer(kernelHandle, "Data", computeBuffers[_currentBuffer]);
-
-            _erosionCompute.SetBuffer(kernelHandle, "UpdatedData", computeBuffers[(_currentBuffer + 1) % 2]);
-
-            // Dispatch the compute shader
-            _erosionCompute.Dispatch(kernelHandle, _mapSize / 16, _mapSize / 16, 1);
-
-            // Swap the buffers
-            _currentBuffer = (_currentBuffer + 1) % 2;
-        }
-
-        computeBuffers[_currentBuffer].GetData(_cellDataBuffers[_currentBuffer]);
-        computeBuffers[(_currentBuffer + 1) % 2].GetData(_cellDataBuffers[(_currentBuffer + 1) % 2]);
-        computeBuffers[_currentBuffer].Release();
-        computeBuffers[(_currentBuffer + 1) % 2].Release();
-
-        for (int i = 0; i < 5; i++)
-            Debug.Log(_cellDataBuffers[(_currentBuffer + 1) % 2][i + _mapSize].ToString());
-
-        _heightmap2D = new float[_mapSize, _mapSize];
-
-        Parallel.For(0, _mapSize, i =>
-        {
-            for (int j = 0; j < _mapSize; j++)
-            {
-                _heightmap2D[i, j] = _cellDataBuffers[(_currentBuffer + 1) % 2][i * _mapSize + j].Height;
             }
         });
     }
